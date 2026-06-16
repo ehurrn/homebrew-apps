f5a5489f0945537a953fe3ef1f4ab76266a8d9f1
ehurrn <97811715+ehurrn@users.noreply.github.com>
2026-06-16 17:34:11 -0500

    Update moc.rb
    Fix `depends_on` error, format was deprecated.  Also fixed change 
    
    
    
    ```
    Warning: Calling string comparison format for `depends_on macos:` is deprecated! Use `depends_on macos: :monterey` instead.
    Please report this issue to the voltangle/homebrew-apps tap (not Homebrew/* repositories), or even better, submit a PR to fix it:
      /opt/homebrew/Library/Taps/voltangle/homebrew-apps/Casks/moc.rb:4
    ```

--- Casks/moc.rb
+++ Casks/moc.rb
@@ -1,7 +1,7 @@
 cask "moc" do
   version "0.2.0"
   sha256 "b57485c390f60cd766b59fbb31587ab56980bdf71e94a2f76dcc7b43fc024cba"
-  depends_on macos: ">= :monterey"
+  depends_on macos: :monterey
   auto_updates true
 
   url "https://github.com/mock-foundation/moc/releases/download/#{version}/Moc.dmg"
@@ -11,5 +11,5 @@ cask "moc" do
 
   app "Moc.app"
 
-  zap trash: "~/Library/Containers/dev.ggoraa.moc"
+  zap trash: "~/Library/Containers/dev.voltangle.moc"
 end
